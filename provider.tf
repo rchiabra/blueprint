cfbe8a014da737d14ca804148d105b4bd05194e4
rchiabra <rchiabra@pe.ibm.com>
2020-05-06 11:56:50 -0500

    Updated

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "ibm" {
 #  generation            = 2
-  region                = var.ibmcloud_region
-  ibmcloud_api_key      = var.ibmcloud_api_key
+#  region                = var.ibmcloud_region
+#  ibmcloud_api_key      = var.ibmcloud_api_key
 }
