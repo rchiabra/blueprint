b0960cbc393a9d25237fcd828a29ab795386f9f7
rchiabra <rchiabra@pe.ibm.com>
2020-05-06 09:08:27 -0500

    First Commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+#######################################
+# Create the resource group
+#######################################
+resource "ibm_resource_group" "group" {
+  name     = var.environment_name
+}
+
+#######################################
+# Create services in the resource group
+#######################################
+# a database
+resource "ibm_resource_instance" "database" {
+    name              = "blueprint_database"
+    service           = "cloudantnosqldb"
+    plan              = var.cloudantnosqldb_plan
+    location          = var.cloudantnosqldb_location
+    resource_group_id = ibm_resource_group.group.id
+}
+# a cloud object storage
+resource "ibm_resource_instance" "objectstorage" {
+    name              = "blueprint_objectstorage"
+    service           = "cloud-object-storage"
+    plan              = var.cloudobjectstorage_plan
+    location          = var.cloudobjectstorage_location
+    resource_group_id = ibm_resource_group.group.id
+}
+
+# a LogDNA service
+resource "ibm_resource_instance" "logging" {
+    name              = "blueprint_logging"
+    service           = "logdna"
+    plan              = var.logdna_plan
+    location          = var.logdna_location
+    resource_group_id = ibm_resource_group.group.id
+}
---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "ibmcloud_api_key" {}
+
+variable "ibmcloud_region" {
+    description = "Preferred IBM Cloud region to use for your infrastructure"
+    default = "us-east"
+}
+variable "environment_name" {
+    description = "resource group name"
+    default = "blueprint_base"
+}
+
+variable "cloudantnosqldb_plan" {
+    description = "Plan for Cloudant database: lite or standard"
+    default="standard"
+}
+
+variable "cloudantnosqldb_location" {
+    description = "Find available locations with: ibmcloud catalog service cloudantnosqldb"
+    default = "us-east"
+}
+
+variable "cloudobjectstorage_plan" {
+    description = "Plan for Cloud Object Storage: lite or standard"
+    default="standard"
+}
+
+variable "cloudobjectstorage_location" {
+    description = "Find available locations with: ibmcloud catalog service cloud-object-storage"
+    default = "global"
+}
+
+variable "logdna_plan" {
+    description = "Plan for LogDNA: lite, 7-day,14-day, 30-day"
+    default="lite"
+}
+
+variable "logdna_location" {
+    description = "Find available locations with: ibmcloud catalog service logdna"
+    default = "us-east"
+}
